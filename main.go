1deb5a1735dde0e6a36b7097942eca3d5a8cd05f
Dan Sosedoff <dan.sosedoff@gmail.com>
2014-07-05 18:14:47 -0500

    Add options

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
@@ -17,8 +18,13 @@ var options struct {
 var services []Service
 
 func main() {
-	options.Path = "./config"
-	options.Port = 5000
+	flag.StringVar(&options.Path, "c", "", "Path to config directory")
+	flag.IntVar(&options.Port, "p", 3050, "Port to listen on")
+	flag.Parse()
+
+	if options.Path == "" {
+		options.Path = "./config"
+	}
 
 	var err error
 	services, err = readServices()
@@ -29,6 +35,7 @@ func main() {
 	}
 
 	fmt.Printf("envd v%s\n", VERSION)
+	fmt.Println("config path:", options.Path)
 	fmt.Println("services detected:", len(services))
 	fmt.Println("starting server on port", options.Port)
 
