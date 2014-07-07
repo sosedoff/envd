86de230d1e8e0066ec9e9f64dac8998c30ef8d5e
Dan Sosedoff <dan.sosedoff@gmail.com>
2014-07-07 13:57:13 -0500

    Add read hosts for the environment

--- environment_test.go
+++ environment_test.go
@@ -30,6 +30,7 @@ func Test_readEnvironments(t *testing.T) {
 	assert.Equal(t, envs[0].Name, "production")
 	assert.Equal(t, len(envs[0].Keys), 4)
 	assert.Equal(t, len(envs[0].Hosts), 3)
+	assert.Equal(t, envs[0].Hosts, []string{"192.168.1.1", "192.168.1.2", "192.168.1.3"})
 	assert.Equal(t, envs[0].Token, "sampletoken")
 
 	assert.Equal(t, envs[1].Name, "staging")
